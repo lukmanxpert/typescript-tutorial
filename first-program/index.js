282516c7a9042b407756d52f9bfcb765e668bd0f
lukmanxpert <lukmanmiah2004@gmail.com>
2025-07-04 01:58:45 +0600

    first commit

---
+++ first-program/index.js
@@ -0,0 +1,4 @@
+var sum = function (num1, num2) {
+    console.log("sum of numbers is ", num1 + num2);
+};
+sum(5, 10);
---
+++ first-program/index.ts
@@ -0,0 +1,5 @@
+const sum = (num1: number, num2: number) => {
+  console.log("sum of numbers is ", num1 + num2);
+};
+
+sum(5, 10);
