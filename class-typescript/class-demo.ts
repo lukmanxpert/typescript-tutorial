5fb1e3e50e9d2709d90f14413af3768e41d2ca85
lukmanxpert <lukmanmiah2004@gmail.com>
2025-07-06 20:57:14 +0600

    explore more about class

--- class-typescript/class-demo.ts
+++ class-typescript/class-demo.ts
@@ -15,3 +15,21 @@ user1.display();
 
 let user2 = new User("Sheikh Shorif", 11);
 user2.display();
+
+class Product {
+  name: string;
+  price: number;
+  constructor(productName: string, productPrice: number) {
+    this.name = productName;
+    this.price = productPrice;
+  }
+  run() {
+    console.log(`product name: ${this.name} and product price: ${this.price}`);
+  }
+}
+
+const product1 = new Product("Mac", 999);
+const product2 = new Product("Laptop HP", 500);
+
+product1.run();
+product2.run();
