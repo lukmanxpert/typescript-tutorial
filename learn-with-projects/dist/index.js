b50a1d57d064db1d0b45bd49aa3275f8e70aa029
lukmanxpert <lukmanmiah2004@gmail.com>
2025-07-07 20:40:02 +0600

    dom manipulation in typescript

--- learn-with-projects/dist/index.js
+++ learn-with-projects/dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("Iam typescript");
+const userForm = document.querySelector(".user-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+userForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = nameInput.value;
+    const email = emailInput.value;
+    const userData = {
+        name,
+        email,
+    };
+    console.log(userData);
+});
--- learn-with-projects/src/index.ts
+++ learn-with-projects/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Iam typescript");
+const userForm = document.querySelector(".user-form") as HTMLFormElement;
+
+const nameInput = document.querySelector("#name") as HTMLInputElement;
+const emailInput = document.querySelector("#email") as HTMLInputElement;
+
+userForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const userData = {
+    name,
+    email,
+  };
+  console.log(userData);
+});
