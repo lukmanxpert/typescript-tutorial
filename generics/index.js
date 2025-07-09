cdc394b0682c4ffcdf89479a60e3e3ac2ded742e
lukmanxpert <lukmanmiah2004@gmail.com>
2025-07-09 15:20:47 +0600

    generics

---
+++ generics/index.js
@@ -0,0 +1,9 @@
+function UserInfo(name, age) {
+    console.log(name, age);
+}
+UserInfo("Sheikh Lukman", 21);
+UserInfo("Sheikh Shorif", 10);
+UserInfo("Sheikh Tisha", 16);
+UserInfo("Sheikh Lima", 26);
+UserInfo("Sheikh Liza", 27);
+UserInfo("Sheikh Masum", 20);
---
+++ generics/index.ts
@@ -0,0 +1,10 @@
+function UserInfo<x, y>(name: x, age: y) {
+  console.log(name, age);
+}
+
+UserInfo("Sheikh Lukman", 21);
+UserInfo("Sheikh Shorif", 10);
+UserInfo("Sheikh Tisha", 16);
+UserInfo("Sheikh Lima", 26);
+UserInfo("Sheikh Liza", 27);
+UserInfo("Sheikh Masum", 20);
